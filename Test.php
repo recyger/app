57b9f34ef86e5ff6dd189b36bd55c95f2d9d1b9c
fuoco <recyger@gmail.com>
2016-04-01 18:21:59 +0300

    init

---
+++ Test.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace testing;
+
+
+class Test
+{
+    public function testing(){
+        (new Client())->testing();
+        echo 'test pass' . PHP_EOL;
+    }
+}
---
+++ app.php
@@ -0,0 +1,8 @@
+<?php
+
+require(__DIR__ . '/vendor/autoload.php');
+require(__DIR__ . '/Test.php');
+use testing\Test;
+
+$test = new Test();
+$test->testing();
